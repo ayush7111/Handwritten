86de3b5d5e8a3f1234aa4a14a1cb306082da1745
spec-rahul <84712129+spec-rahul@users.noreply.github.com>
2022-07-03 11:52:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ with st.expander("See explanation"):
         * 80% Data is used for Training and 20 % Data is used of Testing purpose
      """)
 #loading the saved model
-model = load_model("./data/text_model.h5")
+model = load_model("./data/model.pkl")
 
 st.subheader('')
 st.subheader('')
@@ -77,19 +77,19 @@ if uploaded_file is not None:
      st.image(image, caption='Input Image', width=200)
      img_path = os.path.join("./data/test", uploaded_file.name)
      img_array =cv2.imread(img_path)
-     alpha_dict = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X', 24:'Y',25:'Z'}
+     decode={0:'ka' ,1:'kha' ,2:'ga',3:'gha',4:'kna',5:'cha' }
      gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
      gray = cv2.medianBlur(gray, 5)
      ret, gray = cv2.threshold(gray, 75, 180, cv2.THRESH_BINARY)
      element = cv2.getStructuringElement(cv2.MORPH_RECT, (90, 90))
      gray = cv2.morphologyEx(gray, cv2.MORPH_GRADIENT, element)
      gray = gray / 255.  # downsampling
-     gray = cv2.resize(gray, (28, 28))  # resizing
+     gray = cv2.resize(gray, (32, 32))  # resizing
      # reshaping the image
-     gray = np.reshape(gray, (28, 28))
+     gray = np.reshape(gray, (32, 32))
 
     #--> Displayong Result
-     pred = alpha_dict[np.argmax(model.predict(np.reshape(gray, (1, 28, 28, 1))))]
+     pred = decode[np.argmax(model.predict(np.reshape(gray, (1, 28, 28, 1))))]
      st.subheader('')
      st.markdown('<p class="big-font">Predicted as  - ></p>', unsafe_allow_html=True)
      st.write(pred)
