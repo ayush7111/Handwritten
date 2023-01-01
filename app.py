c3cda2c1ae90d1d50a4c86d526ade03749be4f84
ayush7111 <85789310+ayush7111@users.noreply.github.com>
2023-01-01 09:15:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ if uploaded_file is not None:
      with open(os.path.join("./data/test", uploaded_file.name), "wb") as f:
          f.write(uploaded_file.getbuffer())
      image = Image.open(uploaded_file)
-     st.image(image, caption='Input Image',width=50)
+     st.image(image, caption='Input Image',width=300)
      img_path = os.path.join("./data/test", uploaded_file.name)
      img_array =cv2.imread(img_path)
      alpha_dict = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X', 24:'Y',25:'Z'}
